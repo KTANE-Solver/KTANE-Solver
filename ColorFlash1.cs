7bcbd863ec0618e3c47703c5945812881a80eda0
Nya Bentley <nyabentley@gmail.com>
2022-09-18 09:54:52 -0400

    Inplemented Color Flash

---
+++ ColorFlash1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_KTANE_Solver
+{
+    public class ColorFlash1 : Module
+    {
+        string lastColor;
+        public ColorFlash1(string lastColor,StreamWriter logFileWriter) : base(null, logFileWriter, "Color Flash")
+        {
+            this.lastColor = lastColor;
+        }
+
+        public string Solve(bool debug)
+        {
+            string answer;
+            switch (lastColor)
+            {
+                case "Red":
+                    if (MessageBox.Show("Is green is used as the word at least three times in the sequence?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press Yes on the third time Green is used as either the word or the colour of the word in the sequence";
+                    }
+
+                    else if (MessageBox.Show("Is Blue is used as the colour of the word exactly once?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press No when the word Magenta is shown";
+                    }
+
+                    else
+                    {
+                        answer = "press Yes the last time White is either the word or the colour of the word in the sequence";
+                    }
+
+                    break;
+
+                case "Yellow":
+                    if (MessageBox.Show("Is the word Blue is shown in Green colour?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press Yes on the third time Green is used as either the word or the colour of the word in the sequence";
+                    }
+
+                    else if (MessageBox.Show("Is the word White is shown in either White or Red colour?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press Yes on the second time in the sequence where the colour of the word does not match the word itself";
+                    }
+
+                    else
+                    {
+                        answer = "count the number of times Magenta is used as either the word or the colour of the word in the sequence";
+                    }
+
+                    break;
+
+                case "Green":
+                    if (MessageBox.Show("Is a word occurs consecutively with different colours?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press No on the fifth entry in the sequence";
+                    }
+
+                    else if (MessageBox.Show("Is Magenta is used as the word at least three times in the sequence?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press No on the first time Yellow is used as either the word or the colour of the word in the sequence";
+                    }
+
+                    else
+                    {
+                        answer = "press Yes on any colour where the colour of the word matches the word itself";
+                    }
+                    break ;
+
+                case "Blue":
+                    if (MessageBox.Show("Is the colour of the word does not match the word itself three times or more in the sequence?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press Yes on the first time in the sequence where the colour of the word does not match the word itself";
+                    }
+
+                    else if (MessageBox.Show("Is the word Red is shown in Yellow colour or the word Yellow is shown in White colour?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press No when the word White is shown in Red colour";
+                    }
+
+                    else
+                    {
+                        answer = "press Yes the last time Green is either the word or the colour of the word in the sequence";
+                    }
+                    break;
+
+                case "Magenta":
+                    if (MessageBox.Show("Is a colour occurs consecutively with different words?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press Yes on the third entry in the sequence";
+                    }
+
+                    else if (MessageBox.Show("Is the number of times the word Yellow appears is greater than the number of times that the colour of the word is Blue?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press No the last time the word Yellow is in the sequence";
+                    }
+
+                    else
+                    {
+                        answer = "press No on the first time in the sequence where the colour of the word matches the word of the seventh entry in the sequence";
+                    }
+                    break;
+
+                    default:
+                    if (MessageBox.Show("Is the colour of the third word matches the word of the fourth word or fifth word?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press No the first time that Blue is used as the word or the colour of the word in the sequence";
+                    }
+
+                    else if (MessageBox.Show("Is the word Yellow is shown in Red colour?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        answer = "press Yes on the last time Blue is used as the colour of the word";
+                    }
+
+                    else
+                    {
+                        answer = "press No";
+                    }
+                    break;
+            }
+
+            if (!debug)
+            {
+                ShowAnswer(answer, true);
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ ColorFlash2.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_KTANE_Solver
+{
+    public class ColorFlash2 : Module
+    {
+        List<string> colors;
+        List<string> words;
+
+        public ColorFlash2(List<string> colors, List<string> words, StreamWriter logFileWriter) : base(null, logFileWriter, "Color Flash")
+        {
+            this.colors = colors;
+            this.words = words;
+        }
+
+        public KeyValuePair<string, int> Solve(bool debug)
+        {
+            int answer = -1;
+            string button = "Yes";
+
+            switch (colors[7])
+            {
+                case "Red":
+                    if (words.Where(x => x == "G").Count() >= 3)
+                    {
+                        answer = WordColorIndexes("G")[2] + 1;
+                        button = "Yes";
+                    }
+
+                    else if (words.Where(x => x == "B").Count() == 1)
+                    {
+                        answer = words.IndexOf("M");
+                        button = "No";
+
+                    }
+
+                    else
+                    { 
+                        answer = WordColorIndexes("W").Last() + 1;
+                        button = "Yes";
+                    }
+                    break;
+
+                case "Yellow":
+
+                    if (FindColorAndWord("G", "B") != -1)
+                    {
+                        answer = colors.IndexOf("G");
+                        button = "Yes";
+                    }
+
+                    else if (FindColorAndWord("R", "W") != -1 || FindColorAndWord("W", "W") != -1)
+                    {
+                        answer = MisMatchIndex()[1];
+                        button = "Yes";
+                    }
+
+                    else
+                    {
+                        answer = WordColorIndexes("M").Count();
+                        button = "No";
+                    }
+
+                    break;
+
+
+                case "Green":
+
+                    for (int i = 0; i < 7; i++)
+                    {
+                        int index = ConsecutivePair(words, i);
+                        if (index != -1 && colors[index] != colors[index + 1])
+                        {
+                            answer = 5;
+                            button = "No";
+                            break;
+                        }
+                    }
+
+                    if (answer == -1)
+                    {
+                        if (words.Where(x => x == "M").Count() >= 3)
+                        {
+                            int colorYellow = colors.IndexOf("Y") + 1;
+                            int wordYellow = colors.IndexOf("Y") + 1;
+
+                            if (colorYellow != -1 && colorYellow < wordYellow)
+                            {
+                                button = "No";
+                                answer = colorYellow;
+                            }
+
+                            else
+                            {
+                                button = "No";
+                                answer = wordYellow;
+                            }
+                        }
+
+                        else
+                        {
+                            for (int i = 0; i < 8; i++)
+                            {
+                                if (colors[i] == words[i])
+                                {
+                                    button = "Yes";
+                                    answer = i + 1;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+
+                case "Blue":
+                    int[] misMatches = MisMatchIndex();
+                    if (misMatches.Length >= 3)
+                    {
+                        button = "Yes";
+                        answer = misMatches[0] + 1;
+                    }
+
+                    else if (FindColorAndWord("Y", "R") != -1 || FindColorAndWord("W", "Y") != -1)
+                    {
+                        button = "No";
+                        answer = FindColorAndWord("R", "W") + 1;
+                    }
+
+                    else
+                    {
+                        button = "Yes";
+                        answer = WordColorIndexes("G").Last() + 1;
+                    }
+                    break;
+
+                case "Magenta":
+                    for (int i = 0; i < 7; i++)
+                    {
+                        int index = ConsecutivePair(colors, i);
+                        if (index != -1 && words[index] != words[index + 1])
+                        {
+                            answer = 3;
+                            break;
+                        }
+                    }
+
+                    if (answer == -1)
+                    {
+                        List<int> yellowWords = new List<int>();
+
+                        for (int i = 0; i < 8; i++)
+                        {
+                            if (words[i] == "Y")
+                            {
+                                button = "Yes";
+                                yellowWords.Add(i);
+                            }
+                        }
+                        if (yellowWords.Count > words.Where(x => x == "B").Count())
+                        {
+                            button = "No";
+                            answer = yellowWords.Last();
+                        }
+
+                        else
+                        { 
+                            string desiredColor = words[7];
+
+                            button = "No";
+                            answer = colors.IndexOf(desiredColor);
+                        }
+                    }
+
+                    break;
+
+                default:
+                    if (colors[2] == colors[3] || colors[2] == colors[4])
+                    {
+                        button = "No";
+                        answer = WordColorIndexes("B").First();
+                    }
+
+                    else if (FindColorAndWord("R", "Y") == -1)
+                    {
+                        for (int i = 7; i > 0; i--)
+                        {
+                            if (colors[i] == "B")
+                            {
+                                button = "No";
+                                answer = i;
+                                break;
+                            }
+                        }
+                    }
+
+                    else
+                    {
+                        button = "Yes";
+                        answer = -1;
+                    }
+
+                    break;
+
+            }
+
+            answer++;
+
+            if (!debug)
+            { 
+                
+            }
+
+            return new KeyValuePair<string, int>(button, answer);
+        }
+
+        private int ConsecutivePair(List<string> list, int startingIndex)
+        {
+            return list[startingIndex] == list[startingIndex + 1] ? startingIndex : -1;
+        }
+
+        private int[] MisMatchIndex()
+        {
+            List<int> num = new List<int>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (colors[i] != words[i])
+                {
+                    num.Add(i);
+                }
+            }
+
+            return num.ToArray();
+        }
+
+        private int FindColorAndWord(string color, string word)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (colors[i] == color && words[i] == word)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private int[] WordColorIndexes(string s)
+        {
+            List<int> index = new List<int>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (words[i] == s || colors[i] == s)
+                {
+                    index.Add(i);
+                }
+            }
+
+            return index.ToArray();
+        }
+
+        private string FindAnswerIndex(int answer)
+        {
+            if (answer == -1)
+            {
+                return "any";
+            }
+
+            if (answer == 1)
+            {
+                return "1st";
+            }
+
+            if (answer == 2)
+            {
+                return "2nd";
+            }
+
+            if (answer == 3)
+            {
+                return "3rd";
+            }
+
+            else
+            {
+                return answer + "th";
+            }
+        }
+    }
+}
