891985a5075e49c8a176eb787a2fb99084699945
Nya <nyabentley@gmail.com>
2022-12-16 16:17:29 -0500

    Fixed the edgework confrimation form so that all the information appears without the need to resize the form on load

--- EdgeworkConfirmationForm.Designer.cs
+++ EdgeworkConfirmationForm.Designer.cs
@@ -60,18 +60,19 @@
             // portPlateNumLabel
             // 
             this.portPlateNumLabel.AutoSize = true;
-            this.portPlateNumLabel.Location = new System.Drawing.Point(150, 493);
+            this.portPlateNumLabel.Location = new System.Drawing.Point(131, 370);
             this.portPlateNumLabel.Name = "portPlateNumLabel";
-            this.portPlateNumLabel.Size = new System.Drawing.Size(85, 20);
+            this.portPlateNumLabel.Size = new System.Drawing.Size(68, 15);
             this.portPlateNumLabel.TabIndex = 54;
             this.portPlateNumLabel.Text = "Port Plate #";
             this.portPlateNumLabel.UseWaitCursor = true;
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(72, 595);
+            this.backButton.Location = new System.Drawing.Point(50, 575);
+            this.backButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(69, 46);
+            this.backButton.Size = new System.Drawing.Size(60, 34);
             this.backButton.TabIndex = 53;
             this.backButton.Text = "Back";
             this.backButton.UseVisualStyleBackColor = true;
@@ -79,9 +80,10 @@
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(233, 595);
+            this.submitButton.Location = new System.Drawing.Point(191, 575);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(69, 46);
+            this.submitButton.Size = new System.Drawing.Size(60, 34);
             this.submitButton.TabIndex = 52;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
@@ -90,224 +92,224 @@
             // indicatorLabel
             // 
             this.indicatorLabel.AutoSize = true;
-            this.indicatorLabel.Location = new System.Drawing.Point(147, 130);
+            this.indicatorLabel.Location = new System.Drawing.Point(129, 98);
             this.indicatorLabel.Name = "indicatorLabel";
-            this.indicatorLabel.Size = new System.Drawing.Size(68, 20);
+            this.indicatorLabel.Size = new System.Drawing.Size(54, 15);
             this.indicatorLabel.TabIndex = 51;
             this.indicatorLabel.Text = "Indicator";
             // 
             // portLabel
             // 
             this.portLabel.AutoSize = true;
-            this.portLabel.Location = new System.Drawing.Point(150, 433);
+            this.portLabel.Location = new System.Drawing.Point(131, 325);
             this.portLabel.Name = "portLabel";
-            this.portLabel.Size = new System.Drawing.Size(35, 20);
+            this.portLabel.Size = new System.Drawing.Size(29, 15);
             this.portLabel.TabIndex = 50;
             this.portLabel.Text = "Port";
             // 
             // dayOfWeekLabel
             // 
             this.dayOfWeekLabel.AutoSize = true;
-            this.dayOfWeekLabel.Location = new System.Drawing.Point(127, 26);
+            this.dayOfWeekLabel.Location = new System.Drawing.Point(111, 20);
             this.dayOfWeekLabel.Name = "dayOfWeekLabel";
-            this.dayOfWeekLabel.Size = new System.Drawing.Size(93, 20);
+            this.dayOfWeekLabel.Size = new System.Drawing.Size(73, 15);
             this.dayOfWeekLabel.TabIndex = 49;
             this.dayOfWeekLabel.Text = "Day of Week";
             // 
             // stereoLabel
             // 
             this.stereoLabel.AutoSize = true;
-            this.stereoLabel.Location = new System.Drawing.Point(134, 675);
+            this.stereoLabel.Location = new System.Drawing.Point(117, 506);
             this.stereoLabel.Name = "stereoLabel";
-            this.stereoLabel.Size = new System.Drawing.Size(74, 20);
+            this.stereoLabel.Size = new System.Drawing.Size(58, 15);
             this.stereoLabel.TabIndex = 48;
             this.stereoLabel.Text = "stereo rca";
             // 
             // serialLabel
             // 
             this.serialLabel.AutoSize = true;
-            this.serialLabel.Location = new System.Drawing.Point(150, 638);
+            this.serialLabel.Location = new System.Drawing.Point(131, 478);
             this.serialLabel.Name = "serialLabel";
-            this.serialLabel.Size = new System.Drawing.Size(44, 20);
+            this.serialLabel.Size = new System.Drawing.Size(34, 15);
             this.serialLabel.TabIndex = 47;
             this.serialLabel.Text = "serial";
             // 
             // rjLabel
             // 
             this.rjLabel.AutoSize = true;
-            this.rjLabel.Location = new System.Drawing.Point(147, 617);
+            this.rjLabel.Location = new System.Drawing.Point(129, 463);
             this.rjLabel.Name = "rjLabel";
-            this.rjLabel.Size = new System.Drawing.Size(40, 20);
+            this.rjLabel.Size = new System.Drawing.Size(31, 15);
             this.rjLabel.TabIndex = 46;
             this.rjLabel.Text = "rj-45";
             // 
             // psLabel
             // 
             this.psLabel.AutoSize = true;
-            this.psLabel.Location = new System.Drawing.Point(149, 578);
+            this.psLabel.Location = new System.Drawing.Point(130, 434);
             this.psLabel.Name = "psLabel";
-            this.psLabel.Size = new System.Drawing.Size(38, 20);
+            this.psLabel.Size = new System.Drawing.Size(30, 15);
             this.psLabel.TabIndex = 45;
             this.psLabel.Text = "ps/2";
             // 
             // parallelLabel
             // 
             this.parallelLabel.AutoSize = true;
-            this.parallelLabel.Location = new System.Drawing.Point(137, 544);
+            this.parallelLabel.Location = new System.Drawing.Point(120, 408);
             this.parallelLabel.Name = "parallelLabel";
-            this.parallelLabel.Size = new System.Drawing.Size(57, 20);
+            this.parallelLabel.Size = new System.Drawing.Size(45, 15);
             this.parallelLabel.TabIndex = 44;
             this.parallelLabel.Text = "Parallel";
             // 
             // dvidLabel
             // 
             this.dvidLabel.AutoSize = true;
-            this.dvidLabel.Location = new System.Drawing.Point(147, 510);
+            this.dvidLabel.Location = new System.Drawing.Point(129, 382);
             this.dvidLabel.Name = "dvidLabel";
-            this.dvidLabel.Size = new System.Drawing.Size(50, 20);
+            this.dvidLabel.Size = new System.Drawing.Size(38, 15);
             this.dvidLabel.TabIndex = 43;
             this.dvidLabel.Text = "DVI-D";
             // 
             // emptyPortPlateLabel
             // 
             this.emptyPortPlateLabel.AutoSize = true;
-            this.emptyPortPlateLabel.Location = new System.Drawing.Point(119, 472);
+            this.emptyPortPlateLabel.Location = new System.Drawing.Point(104, 354);
             this.emptyPortPlateLabel.Name = "emptyPortPlateLabel";
-            this.emptyPortPlateLabel.Size = new System.Drawing.Size(118, 20);
+            this.emptyPortPlateLabel.Size = new System.Drawing.Size(95, 15);
             this.emptyPortPlateLabel.TabIndex = 42;
             this.emptyPortPlateLabel.Text = "Empty Port Plate";
             // 
             // trnLabel
             // 
             this.trnLabel.AutoSize = true;
-            this.trnLabel.Location = new System.Drawing.Point(122, 400);
+            this.trnLabel.Location = new System.Drawing.Point(107, 300);
             this.trnLabel.Name = "trnLabel";
-            this.trnLabel.Size = new System.Drawing.Size(100, 20);
+            this.trnLabel.Size = new System.Drawing.Size(79, 15);
             this.trnLabel.TabIndex = 41;
             this.trnLabel.Text = "Indicator TRN";
             // 
             // sndLabel
             // 
             this.sndLabel.AutoSize = true;
-            this.sndLabel.Location = new System.Drawing.Point(119, 378);
+            this.sndLabel.Location = new System.Drawing.Point(104, 284);
             this.sndLabel.Name = "sndLabel";
-            this.sndLabel.Size = new System.Drawing.Size(102, 20);
+            this.sndLabel.Size = new System.Drawing.Size(80, 15);
             this.sndLabel.TabIndex = 40;
             this.sndLabel.Text = "Indicator SND";
             // 
             // sigLabel
             // 
             this.sigLabel.AutoSize = true;
-            this.sigLabel.Location = new System.Drawing.Point(125, 358);
+            this.sigLabel.Location = new System.Drawing.Point(109, 268);
             this.sigLabel.Name = "sigLabel";
-            this.sigLabel.Size = new System.Drawing.Size(94, 20);
+            this.sigLabel.Size = new System.Drawing.Size(74, 15);
             this.sigLabel.TabIndex = 39;
             this.sigLabel.Text = "Indicator SIG";
             // 
             // nsaLabel
             // 
             this.nsaLabel.AutoSize = true;
-            this.nsaLabel.Location = new System.Drawing.Point(121, 337);
+            this.nsaLabel.Location = new System.Drawing.Point(106, 253);
             this.nsaLabel.Name = "nsaLabel";
-            this.nsaLabel.Size = new System.Drawing.Size(101, 20);
+            this.nsaLabel.Size = new System.Drawing.Size(80, 15);
             this.nsaLabel.TabIndex = 38;
             this.nsaLabel.Text = "Indicator NSA";
             // 
             // indLabel
             // 
             this.indLabel.AutoSize = true;
-            this.indLabel.Location = new System.Drawing.Point(125, 293);
+            this.indLabel.Location = new System.Drawing.Point(109, 220);
             this.indLabel.Name = "indLabel";
-            this.indLabel.Size = new System.Drawing.Size(98, 20);
+            this.indLabel.Size = new System.Drawing.Size(77, 15);
             this.indLabel.TabIndex = 37;
             this.indLabel.Text = "Indicator IND";
             // 
             // msaLabel
             // 
             this.msaLabel.AutoSize = true;
-            this.msaLabel.Location = new System.Drawing.Point(118, 315);
+            this.msaLabel.Location = new System.Drawing.Point(103, 236);
             this.msaLabel.Name = "msaLabel";
-            this.msaLabel.Size = new System.Drawing.Size(103, 20);
+            this.msaLabel.Size = new System.Drawing.Size(82, 15);
             this.msaLabel.TabIndex = 36;
             this.msaLabel.Text = "Indicator MSA";
             // 
             // frqLabel
             // 
             this.frqLabel.AutoSize = true;
-            this.frqLabel.Location = new System.Drawing.Point(118, 272);
+            this.frqLabel.Location = new System.Drawing.Point(103, 204);
             this.frqLabel.Name = "frqLabel";
-            this.frqLabel.Size = new System.Drawing.Size(99, 20);
+            this.frqLabel.Size = new System.Drawing.Size(79, 15);
             this.frqLabel.TabIndex = 35;
             this.frqLabel.Text = "Indicator FRQ";
             // 
             // frkLabel
             // 
             this.frkLabel.AutoSize = true;
-            this.frkLabel.Location = new System.Drawing.Point(118, 252);
+            this.frkLabel.Location = new System.Drawing.Point(103, 189);
             this.frkLabel.Name = "frkLabel";
-            this.frkLabel.Size = new System.Drawing.Size(97, 20);
+            this.frkLabel.Size = new System.Drawing.Size(77, 15);
             this.frkLabel.TabIndex = 34;
             this.frkLabel.Text = "Indicator FRK";
             // 
             // clrLabel
             // 
             this.clrLabel.AutoSize = true;
-            this.clrLabel.Location = new System.Drawing.Point(121, 230);
+            this.clrLabel.Location = new System.Drawing.Point(106, 172);
             this.clrLabel.Name = "clrLabel";
-            this.clrLabel.Size = new System.Drawing.Size(97, 20);
+            this.clrLabel.Size = new System.Drawing.Size(78, 15);
             this.clrLabel.TabIndex = 33;
             this.clrLabel.Text = "Indicator CLR";
             // 
             // carLabel
             // 
             this.carLabel.AutoSize = true;
-            this.carLabel.Location = new System.Drawing.Point(121, 209);
+            this.carLabel.Location = new System.Drawing.Point(106, 157);
             this.carLabel.Name = "carLabel";
-            this.carLabel.Size = new System.Drawing.Size(100, 20);
+            this.carLabel.Size = new System.Drawing.Size(80, 15);
             this.carLabel.TabIndex = 32;
             this.carLabel.Text = "Indicator CAR";
             // 
             // bobLabel
             // 
             this.bobLabel.AutoSize = true;
-            this.bobLabel.Location = new System.Drawing.Point(125, 187);
+            this.bobLabel.Location = new System.Drawing.Point(109, 140);
             this.bobLabel.Name = "bobLabel";
-            this.bobLabel.Size = new System.Drawing.Size(101, 20);
+            this.bobLabel.Size = new System.Drawing.Size(80, 15);
             this.bobLabel.TabIndex = 31;
             this.bobLabel.Text = "Indicator BOB";
             // 
             // batteryHolderLabel
             // 
             this.batteryHolderLabel.AutoSize = true;
-            this.batteryHolderLabel.Location = new System.Drawing.Point(103, 90);
+            this.batteryHolderLabel.Location = new System.Drawing.Point(90, 68);
             this.batteryHolderLabel.Name = "batteryHolderLabel";
-            this.batteryHolderLabel.Size = new System.Drawing.Size(146, 20);
+            this.batteryHolderLabel.Size = new System.Drawing.Size(115, 15);
             this.batteryHolderLabel.TabIndex = 30;
             this.batteryHolderLabel.Text = "# of Battery Holders:";
             // 
             // batteryLabel
             // 
             this.batteryLabel.AutoSize = true;
-            this.batteryLabel.Location = new System.Drawing.Point(129, 69);
+            this.batteryLabel.Location = new System.Drawing.Point(113, 52);
             this.batteryLabel.Name = "batteryLabel";
-            this.batteryLabel.Size = new System.Drawing.Size(98, 20);
+            this.batteryLabel.Size = new System.Drawing.Size(76, 15);
             this.batteryLabel.TabIndex = 29;
             this.batteryLabel.Text = "# of Batteries";
             // 
             // serialNumberLabel
             // 
             this.serialNumberLabel.AutoSize = true;
-            this.serialNumberLabel.Location = new System.Drawing.Point(126, 47);
+            this.serialNumberLabel.Location = new System.Drawing.Point(110, 35);
             this.serialNumberLabel.Name = "serialNumberLabel";
-            this.serialNumberLabel.Size = new System.Drawing.Size(104, 20);
+            this.serialNumberLabel.Size = new System.Drawing.Size(82, 15);
             this.serialNumberLabel.TabIndex = 28;
             this.serialNumberLabel.Text = "Serial Number";
             // 
             // EdgeworkConfirmationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 653);
+            this.ClientSize = new System.Drawing.Size(315, 620);
             this.Controls.Add(this.portPlateNumLabel);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.submitButton);
@@ -335,6 +337,7 @@
             this.Controls.Add(this.batteryHolderLabel);
             this.Controls.Add(this.batteryLabel);
             this.Controls.Add(this.serialNumberLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "EdgeworkConfirmationForm";
             this.Text = "EdgeworkConfirmationForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EdgeworkConfirmationForm_FormClosing);
